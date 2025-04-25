dcf0eabe44d8a618379e496061f9253cb33bbf4f
Joseph Scott <joseph@josephscott.org>
2025-04-24 20:59:26 -0600

    Get into the pings.blo.gs connection

--- server.php
+++ server.php
@@ -1,6 +1,7 @@
 <?php
 declare( strict_types = 1 );
 
+use Workerman\Connection\AsyncTcpConnection;
 use Workerman\Worker;
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -25,6 +26,19 @@ function add_cors_headers( array $headers = [] ): array {
 	return $headers;
 }
 
-$worker->onWorkerStart = function ( $worker ) {
+$worker->onWorkerStart = function ( Worker $worker ) {
+	// Shared connection per worker to ping.blo.gs
+	$worker->blogs_connection = new AsyncTcpConnection( 'tcp://ping.blo.gs:29999' );
 
+	// Data from ping.blo.gs
+	$worker->blogs_connection->onMessage = function(
+		AsyncTcpConnection $connection,
+		$data
+	) use ( $worker ) {
+		$worker->buffer .= $data;
+
+		// We need a complete line
+		while ( ( $pos = strpos( $worker->buffer, "\n" ) ) !== false ) {
+		}
+	}
 };
